90194c32c155b0d57ca68259cab59586db370d31
pricelees <priceelees@gmail.com>
2024-07-21 21:25:07 +0900

    refactor: 일부 도메인에 빠진 GenerationType 지정

--- util/src/main/java/mouda/util/domain/Board.java
+++ util/src/main/java/mouda/util/domain/Board.java
@@ -2,6 +2,7 @@ package mouda.util.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class Board {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String teamName;
